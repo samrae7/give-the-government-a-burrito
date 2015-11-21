0ee8845e3e8f5496ba605aa7d3faf889d007b6cd
Sam Rae <samuelrae@gmail.com>
2015-11-21 11:34:13 +0000

    got list of burritos showing on the page

---
+++ app.js
@@ -0,0 +1,29 @@
+angular.module('jsCodingTest', ['cpLib'
+    // Add module dependencies here.
+]);
+
+angular.module('jsCodingTest').controller('GiveTheGovernmentABurrito', function(PackagesFactory) {
+
+    var self = this
+
+    self.foodType ='burger'
+
+    self.getFoods = function() {
+      PackagesFactory.searchPackages(self.foodType,'SW1A0AA').then(function(response) {
+        self.foods = response.data.packages
+        console.log(self.foods)
+      })
+
+    }
+
+    self.getFoods()
+
+
+
+
+    // Your JavaScript goes here.
+    // Your code should use our JS library's `PackagesFactory.searchPackages` method to search
+    // for burritos that can be delivered to the Houses of Parliament in London.
+    // The API URL that should be called is:
+    // https://api.citypantry.com/packages/search?name=Burrito&postcode=SW1A%200AA
+});
